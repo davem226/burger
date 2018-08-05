8076d4c6b6c25f2d79ee0430b03d87d4d4b727b0
Dave Morris <davem226@yahoo.com>
2018-08-05 00:26:40 -0400

    updated socket path using mysql_config --socket from terminal

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     // port: '/var/run/mysqld/mysqld.sock',
-    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    socketPath: '/tmp/mysql.sock',
     port: 3306,
     user: "bootcamp",
     password: "notarealpassword",
